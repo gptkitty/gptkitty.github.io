e05e8b9cbc4350ee26089f1442519385176ab559
gptkitty <150722183+gptkitty@users.noreply.github.com>
2023-11-18 17:19:38 -0500

    syntax bug

--- search.js
+++ search.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Function to update search results
-    const updateResults = (stories) => {
+    function updateResults(stories) {
         results.innerHTML = '';
         stories.forEach(storyObj => {
             const card = document.createElement('div');
@@ -38,9 +38,9 @@ document.addEventListener('DOMContentLoaded', function() {
             const copyIcon = document.createElement('span');
             copyIcon.classList.add('copy-btn');
             copyIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-  <path fill-rule="evenodd" d="M15.988 3.012A2.25 2.25 0 0118 5.25v6.5A2.25 2.25 0 0115.75 14H13.5V7A2.5 2.5 0 0011 4.5H8.128a2.252 2.252 0 011.884-1.488A2.25 2.25 0 0112.25 1h1.5a2.25 2.25 0 012.238 2.012zM11.5 3.25a.75.75 0 01.75-.75h1.5a.75.75 0 01.75.75v.25h-3v-.25z" clip-rule="evenodd" />
-  <path fill-rule="evenodd" d="M2 7a1 1 0 011-1h8a1 1 0 011 1v10a1 1 0 01-1 1H3a1 1 0 01-1-1V7zm2 3.25a.75.75 0 01.75-.75h4.5a.75.75 0 010 1.5h-4.5a.75.75 0 01-.75-.75zm0 3.5a.75.75 0 01.75-.75h4.5a.75.75 0 010 1.5h-4.5a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
-</svg>`;
+            <path fill-rule="evenodd" d="M15.988 3.012A2.25 2.25 0 0118 5.25v6.5A2.25 2.25 0 0115.75 14H13.5V7A2.5 2.5 0 0011 4.5H8.128a2.252 2.252 0 011.884-1.488A2.25 2.25 0 0112.25 1h1.5a2.25 2.25 0 012.238 2.012zM11.5 3.25a.75.75 0 01.75-.75h1.5a.75.75 0 01.75.75v.25h-3v-.25z" clip-rule="evenodd" />
+            <path fill-rule="evenodd" d="M2 7a1 1 0 011-1h8a1 1 0 011 1v10a1 1 0 01-1 1H3a1 1 0 01-1-1V7zm2 3.25a.75.75 0 01.75-.75h4.5a.75.75 0 010 1.5h-4.5a.75.75 0 01-.75-.75zm0 3.5a.75.75 0 01.75-.75h4.5a.75.75 0 010 1.5h-4.5a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+            </svg>`;
             card.appendChild(copyIcon);
 
             // Click event to copy story
@@ -64,6 +64,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             });
 
+            results.appendChild(card);
+        });
     };
 
     // Function to show toast notification
@@ -76,25 +78,25 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 3000); // Toast disappears after 3 seconds
     }
 
-// Event listener for search box input
-searchBox.addEventListener('input', function() {
-    const searchTerm = searchBox.value.toLowerCase();
+    // Event listener for search box input
+    searchBox.addEventListener('input', function() {
+        const searchTerm = searchBox.value.toLowerCase();
 
-    // Check if the search term is empty
-    if (!searchTerm) {
-        results.innerHTML = ''; // Clear results if search box is empty
-        return; // Exit the function early
-    }
+        // Check if the search term is empty
+        if (!searchTerm) {
+            results.innerHTML = ''; // Clear results if search box is empty
+            return; // Exit the function early
+        }
 
-    // Updated filter to include title, story, and category
-    const filteredStories = userStories.filter(storyObj =>
-        storyObj.title.toLowerCase().includes(searchTerm) ||
-        storyObj.story.toLowerCase().includes(searchTerm) ||
-        storyObj.category.toLowerCase().includes(searchTerm)
-    );
+        // Updated filter to include title, story, and category
+        const filteredStories = userStories.filter(storyObj =>
+            storyObj.title.toLowerCase().includes(searchTerm) ||
+            storyObj.story.toLowerCase().includes(searchTerm) ||
+            storyObj.category.toLowerCase().includes(searchTerm)
+        );
 
-    updateResults(filteredStories);
-});
+        updateResults(filteredStories);
+    });
 
     // Event listeners for filter buttons
     const filterButtons = document.querySelectorAll('.filter-btn');
